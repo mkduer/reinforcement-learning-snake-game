6dcf67ca6db6659a67eb70914773267b1bdf07a9
michelle <michelle.duer@gmail.com>
2018-11-23 17:17:59 -0800

    add actions/directions for ai

--- venv/q_learning.py
+++ venv/q_learning.py
@@ -7,14 +7,41 @@ class Q_Learning:
         # State
         self.actions = {'north': 0, 'east': 0, 'south': 0, 'west': 0}  # "clock-wise" directions
         self.table = {}  # Q-table values
+        self.current_action = 'east'
 
         # Hyperparameters
         self.learning_rate = 1
         self.discount_factor = 0.9
 
+    def move_north(self):
+        if self.current_action == 'north':
+            return True
+        return False
+
+    def move_east(self):
+        if self.current_action == 'east':
+            return True
+        return False
+
+    def move_south(self):
+        if self.current_action == 'south':
+            return True
+        return False
+
+    def move_west(self):
+        if self.current_action == 'west':
+            return True
+        return False
+
+    def next_action(self):
+        self.current_action = 'south'
+
 
     def update(self):
+        # TODO: add q algorithm here
         print(f'update')
+
+
 
 
 def main():
